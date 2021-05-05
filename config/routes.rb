9074d3eb9d49bd5077df74c8156e866e62fa85ae
Regina Casias <reginaannvigil@gmail.com>
2021-05-05 14:22:54 -0700

    Created task manager

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'welcome#index'
+end
+
+Rails.application.routes.draw do
+  get '/', to: 'welcome#index'
+  get '/tasks', to:'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+end
